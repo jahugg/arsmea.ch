3ec385cb29c0f1c547d43d3e13b832cdeb65a481
jahugg <jan@huggenberg.ch>
2021-02-25 11:09:29 +0100

    black and white to color transition

--- src/index.js
+++ src/index.js
@@ -3,20 +3,31 @@ const colorPairs = [
   ['edaab2', '5f543f'],
   ['d19ca6', 'ae530c'],
   ['acbbb8', 'b5934c'],
-  ['b46427', 'c2cccb']
+  ['b46427', 'c2cccb'],
 ];
 let currentColor = 0;
 
 function initApp() {
-  let colorInterval = setInterval(changeColor, 9000);
+  setTimeout(() => setColorPair(getRandomInt(5)), 1000);
+  let colorInterval = setInterval(changeColor, 10000);
+}
+
+function setColorPair(index) {
+  currentColor = index;
+  document.body.style.setProperty('--primary-color', `#${colorPairs[index][0]}`);
+  document.body.style.setProperty('--secondary-color', `#${colorPairs[index][1]}`);
 }
 
 function changeColor() {
   if (currentColor == colorPairs.length - 1) currentColor = 0;
   else currentColor++;
-  
+
   document.body.style.setProperty('--primary-color', `#${colorPairs[currentColor][0]}`);
   document.body.style.setProperty('--secondary-color', `#${colorPairs[currentColor][1]}`);
 }
 
 initApp();
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
